7fb4327dbe99eebc810e5e873120f198c8b8446a
dimtriv <dimastrivianto@gmail.com>
2020-04-14 14:15:05 +0700

    read delete update by id

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const port = 2020
 
 //CONFIG MONGOOSE
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/mongoose-test', {
+mongoose.connect('mongodb://127.0.0.1:27017/mongoose-test', {//mongoose-test nama database
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex : true
 })
 
 //IMPORT MODELS
@@ -23,22 +24,79 @@ app.get('/', (req, res) =>{
     )
 })
 
+//es7
+//Async await
+//try catch
 //Read all user
-app.get('/users', (req,res)=>{
-    User.find({})
-    .then(resp => res.send(resp))
-    .catch(err => res.send(err))
+app.get('/users', async (req, res)=>{
+    
+    try {
+        let users = await User.find({})
+        res.send(users)
+    } catch (err) {
+        res.send(err)
+    }
+    
+    //es6
+    // User.find({})
+    // .then(resp => res.send(resp))
+    // .catch(err => res.send(err))
+})
+
+//Read One User By Id
+//{error : "User dengan id 989999 tidak ditemukan"}
+app.get('/findbyid', async (req, res)=>{
+    try {
+        let id = req.query.id
+        let user = await User.findById(id)
+        res.send(user)
+    } catch (err) {
+        res.send({error: `User dengan id: ${req.query.id} tidak ditemukan`})
+    }
+})
+
+//Update User By Id
+app.patch('/user/:_id', async (req, res)=>{
+    try {
+        let _id = req.params._id
+        let newName = req.body.newname
+        let user = await User.updateOne({_id}, {$set: {name: newName}})
+        res.send(user)
+    } catch (error) {
+        res.send({error: `User dengan id: ${req.params._id} tidak ditemukan`})
+    }
+})
+
+//Delete User By Id
+app.delete('/user/:_id', async (req,res)=>{
+    try {
+        let _id = req.params._id
+        let user = await User.deleteOne({_id})
+        res.send(user)
+    } catch (error) {
+        res.send({error: `User dengan id: ${req.params._id} tidak ditemukan`})
+    }
 })
 
 //Create new user
-app.post('/users', (req,res)=>{
+app.post('/users', async (req,res)=>{
     //req.body ={username: 'dimas', name: 'Dimas', age: 29}
 
     //CREATE NEW USER
     const user = new User(req.body)
 
+    try {
+        let resp = await user.save()
+        res.send(resp)
+    } catch (error) {
+        res.send(error)
+    }
+
+    //ES6
     //SAVE ke database
-    user.save().then(resp => res.send(resp)).catch(err => res.send(err))
+    // user.save().then(resp => res.send(resp)).catch(err => res.send(err))
 })
+
+
 
 app.listen(port, ()=>{console.log('Success Running')})
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,16 +1,59 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 
 //bikin object baru dengan menggunakan keyword new
 const userSchema = new mongoose.Schema({
     username : {
-        type: String
+        type: String,// Mengatur tipe data yang disimpan
+        unique: true,// Tidak boleh duplicate
+        required: true,//Wajib di isi
+        set : (val) => {return val.replace(/ /g, "")},// merubah semua space diantara karakter dengan string kosong
+        validate(value){//Handle jika yang di input user bukan sebuah string, gunanya juga untuk membolehkan atau tidak
+            let result = isNaN(parseInt(value))
+            if(!result){
+                throw new Error("Username tidak boleh angka")
+            }
+        }
     },
     name : {
-        type: String
+        type: String,
+        //harus di isi
+        required: true,
+        trim : true,//menghapus spasi sebelum dan sesudah data input
+        //tidak boleh angka
+        validate(value){
+            let result = isNaN(parseInt(value))
+            if(!result){
+                throw new Error("Name tidak boleh angka")
+            }
+        }
+    },
+    email: {
+        //type string
+        type: String,
+        //wajib ada
+        required: true,
+        //tidak boleh sama
+        unique: true,
+        trim : true,
+        lowercase: true, // akan mengubah data menjadi huruf kecil semua
+        validate(val){
+            if(!validator.isEmail(val)){
+                throw new Error("Email tidak valid")
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7
     },
     age : {
-        type: Number
+        type: Number,
+        default: 0, // default value
+        set: val => parseInt(val)
     }
 })
 
